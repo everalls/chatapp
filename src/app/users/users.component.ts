1d90095e9d1e018ef541b626c1f41cda398f4e6b
everall77 <cadair77@gmail.com>
2018-07-31 21:51:53 +0300

    Side sliding menu bar

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { ChatdataService } from '../chatdata.service';
 import { User } from '../user';
 
+declare var jquery: any;
+declare var $: any;
+
+
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
@@ -9,6 +13,7 @@ import { User } from '../user';
 })
 export class UsersComponent implements OnInit {
 
+  sideBarState = false; // side bar non-visible by default
   users: User[];
   constructor(private chatdataService: ChatdataService) { }
 
@@ -16,6 +21,19 @@ export class UsersComponent implements OnInit {
     this.chatdataService.getUsers()
         .subscribe(users => this.users = users);
   }
+
+ toggleSideNav(): void {
+    const theSideNav = document.getElementById('mySidenav');
+    const currSideNavWidth = theSideNav.clientWidth;
+    let newSideNavWidth = '0px';
+    if (currSideNavWidth > 0) {
+        newSideNavWidth = '0px';
+    } else {
+        newSideNavWidth = '250px';
+    }
+    theSideNav.style.width = newSideNavWidth;
+ }
+
 
   ngOnInit() {
      this.getUsers();
